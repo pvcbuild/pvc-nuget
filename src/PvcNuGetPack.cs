4da8faf218ce0f7aa2cf5fa3e18ded82535b8ac1
Brandon Stirnaman <stirno@gmail.com>
2014-05-20 22:57:11 -0500

    Updated to Pvc.Core 0.0.1.2

--- src/PvcNuGetPack.cs
+++ src/PvcNuGetPack.cs
@@ -1,5 +1,4 @@
-﻿using PvcCore;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -7,6 +6,7 @@ using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Edokan.KaiZen.Colors;
+using PvcCore;
 
 namespace PvcPlugins
 {
